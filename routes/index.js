ea81b1cc2585b228d3e32eb46191667a3dd46f47
Andres Pena <andres.pena.m15@gmail.com>
2023-02-02 16:36:34 -0500

    creando modelos con ORM

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
-const productsRouter = require('./productsRouter');
-const categoriesRouter = require('./categoriesRouter');
-const usersRouter = require('./usersRouter');
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
+const orderRouter = require('./orders.router');
 
 function routerApi(app) {
   const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi(app) {
   router.use('/products', productsRouter);
   router.use('/categories', categoriesRouter);
   router.use('/users', usersRouter);
+  router.use('/orders', orderRouter);
 }
 
 module.exports = routerApi;
